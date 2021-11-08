c1d7fb5db5d505aa4f1d02a55296f8a487a8fcbe
Михаил <egorovmikhail@bk.ru>
2021-11-08 20:49:59 +1100

    Создал пустой проект

---
+++ BlastHoleApp/BlastHoleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlastHoleAppApp.swift
+//  BlastHoleApp
+//
+//  Created by Михаил Егоров on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BlastHoleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlastHoleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlastHoleApp
+//
+//  Created by Михаил Егоров on 08.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
